a58018c1eb0439526b8946e67e14ba04e067c7f8
Artur Assis Guerra <arturassisguerra@gmail.com>
2025-12-17 13:50:58 -0300

    chore(scraper): create product model

---
+++ scraper/src/models/productModel.ts
@@ -0,0 +1,19 @@
+export interface ProductProps {
+    name: string;
+    price: number;
+    image: string;
+    url: string;
+    category: string;
+}
+
+export class Product {
+    public readonly id: string;
+    public readonly createdAt: Date;
+    public readonly props: ProductProps;
+
+    public constructor(props: ProductProps, id?: string, createdAt?: Date) {
+        this.id = id ?? crypto.randomUUID();
+        this.createdAt = createdAt ?? new Date();
+        this.props = props;
+    }
+}
