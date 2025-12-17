f50d6670fd498cdd0a609cba66ecb2a5965946d5
Artur Assis Guerra <arturassisguerra@gmail.com>
2025-12-17 17:41:58 -0300

    feat(scraper): implements savemany in products repository

--- scraper/src/infra/database/dynamoproductsRepository.ts
+++ scraper/src/infra/database/dynamoproductsRepository.ts
@@ -1,6 +1,6 @@
 import { Product } from "@/models/productModel"
 import { ProductRepository } from "@/repositories/productRepository"
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb"
+import { BatchWriteItemCommand, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb"
 import { env } from "../env"
 
 export class DynamoProductsRepository implements ProductRepository {
@@ -21,4 +21,34 @@ export class DynamoProductsRepository implements ProductRepository {
             },
         }));
     }
+
+    async saveMany(products: Product[]): Promise<void> {
+        if (products.length === 0) {
+            return;
+        }
+
+        const putRequests = products.map((product) => ({
+            PutRequest: {
+                Item: {
+                    id: { S: product.id },
+                    name: { S: product.props.name },
+                    price: { N: product.props.price.toString() },
+                    image: { S: product.props.image },
+                    url: { S: product.props.url },
+                    category: { S: product.props.category },
+                    rank: { N: product.props.rank.toString() },
+                    createdAt: { S: product.createdAt.toISOString() },
+                },
+            },
+        }));
+
+        await this.dynamoClient.send(
+            new BatchWriteItemCommand({
+                RequestItems: {
+                    [env.PRODUCTS_TABLE]: putRequests,
+                },
+            }),
+        );
+    }
+
 }
--- scraper/src/repositories/productRepository.ts
+++ scraper/src/repositories/productRepository.ts
@@ -2,4 +2,5 @@ import { Product } from "@/models/productModel"
 
 export interface ProductRepository {
     save(product: Product): Promise<void>;
+    saveMany(products: Product[]): Promise<void>;
 }
